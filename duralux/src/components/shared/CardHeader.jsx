9320f0dfdd452770cd28cdaa8f0295252fe35549
aleynagul <aleynagulkazdal@gmail.com>
2026-01-19 23:20:23 +0300

    dashboard nokta kalıdırldı.

--- duralux/src/components/shared/CardHeader.jsx
+++ duralux/src/components/shared/CardHeader.jsx
@@ -17,85 +17,8 @@ const CardHeader = ({ title, refresh, remove, expanded }) => {
       <h5 className="card-title">{title}</h5>
       <div className="card-header-action">
         <div className="card-header-btn">
-          {/* <div data-toggle="tooltip" data-title="Delete" onClick={remove}>
-            <span
-              className="avatar-text avatar-xs bg-danger"
-              data-bs-toggle="remove"
-            >
-              {" "}
-            </span>
-          </div> */}
-          {/* <div data-toggle="tooltip" data-title="Refresh" onClick={refresh}>
-            <span
-              className="avatar-text avatar-xs bg-warning"
-              data-bs-toggle="refresh"
-            >
-              {" "}
-            </span>
-          </div> */}
-          {/* <div data-toggle="tooltip" data-title="Maximize/Minimize" onClick={expanded}>
-            <span
-              className="avatar-text avatar-xs bg-success"
-              data-bs-toggle="expand"
-            >
-              {" "}
-            </span>
-          </div> */}
         </div>
-        <div className="filter-dropdown">
-          <div
-            className="avatar-text avatar-sm"
-            data-bs-toggle="dropdown"
-            data-bs-offset="25, 25"
-          >
-            <div data-toggle="tooltip" data-title="Options" className="lh-1">
-              <FiMoreVertical />
-            </div>
-          </div>
-          <div className="dropdown-menu dropdown-menu-end">
-            <Link href="#" className="dropdown-item">
-              <i>
-                <FiAtSign />
-              </i>
-             Yeni
-            </Link>
-            {/* <Link href="#" className="dropdown-item">
-              <i>
-              //ıııı
-
-
-              //merhaba dünya
-                <FiCalendar />
-              </i>
-             Olay
-            </Link> */}
-            <Link href="#" className="dropdown-item">
-              <i>
-                <FiBell />
-              </i>
-              Ertelenen
-            </Link>
-            <Link href="#" className="dropdown-item">
-              <i>
-                <FiTrash />
-              </i>
-              Sil
-            </Link>
-            <div className="dropdown-divider"></div>
-            <Link href="#" className="dropdown-item">
-              <i>
-                <FiSettings />
-              </i>
-              Ayarlar
-            </Link>
-            {/* <Link href="#" className="dropdown-item">
-              <i>
-                <FiLifeBuoy />
-              </i>
-              Tips & Tricks
-            </Link> */}
-          </div>
-        </div>
+       
       </div>
     </div>
   );
